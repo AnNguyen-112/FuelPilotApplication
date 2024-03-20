af1399f701808fd950cd73138cd648408d8a8c2e
dalilatn <dalilanguyen11@gmail.com>
2024-03-20 14:41:25 -0500

    update empty file profile - fix

---
+++ Client/src/Components/Profile.js
@@ -0,0 +1,19 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
+const Profile = () => {
+    const { user, isAuthenticated } = useAuth0();
+
+    return (
+        isAuthenticated && (
+            <article className='column'>
+                {user?.picture && <img src={user.picture} alt={user?.name} />}
+                <h2>{user?.name}</h2>
+                <u1>
+                    {Object.keys(user).map((objKey, i) => <li key={i}>{objKey}: {user[objKey]}</li>)}
+                </u1>
+            </article>
+        )
+    )
+}
+
+export default Profile
