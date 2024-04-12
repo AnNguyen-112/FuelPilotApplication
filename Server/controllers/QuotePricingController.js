cd8351970814f9c15b2bb250b122bd95ff848e4a
annguyen <annguyen01122000@gmail.com>
2024-04-12 14:16:28 -0500

    fix date issue and retrieve user address by user email

--- Server/controllers/QuotePricingController.js
+++ Server/controllers/QuotePricingController.js
@@ -2,16 +2,43 @@ const path = require("path");
 
 const { StatusCodes } = require("http-status-codes");
 
+const UserProfile = require('../models/UserProfile');
+const User = require('../models/User')
+
 const postPricingFromQuote = async (req, res) => {
   const unfinishQuote = req.body;
-  console.log(unfinishQuote);
+  // console.log(unfinishQuote);
+
+  // from client:
+  // userEmail: userEmail,
+  // gallonRequested: gallonRequested,           
+  // deliveryDate: deliveryDate,
 
 // get the email from user and calculate the pricing and total amount
 
+//get user adress
+const user = await User.findOne({email: unfinishQuote.userEmail});
+
+if (!user)
+{
+  res.status(StatusCodes.NOT_FOUND).json({message: "User not found"});
+}
+
+const userProfile = await UserProfile.findOne({userId: user._id});
+
+if (!userProfile.address1 || !userProfile.city || !userProfile.state || !userProfile.zipCode)
+{
+   res.status(StatusCodes.BAD_REQUEST).json({message: "Please fill in your physical address in profile management before continue"});
+}
+else 
+
+  userAddress = userProfile.address1 + " " + userProfile.city + ", " +  userProfile.state + ", " + userProfile.zipCode; 
+  
   const requiredDate = new Date(unfinishQuote.deliveryDate);
 
   //validation for post input
- console.log(new Date(unfinishQuote.deliveryDate))
+  // console.log(new Date(unfinishQuote.deliveryDate))
+
   if (
     typeof parseInt(unfinishQuote.gallonsRequested) !== "number" ||
     isNaN(requiredDate.getTime())  
@@ -23,10 +50,9 @@ const postPricingFromQuote = async (req, res) => {
     const Pricing = {
       suggestedPricePerGallon: 50,
       total: 100,
-      userAddress: "9999 NineRoad City State 77072"
+      userAddress: userAddress,
     };
-
-    //return the pricing, can change to a full quote if want
+    
     res.status(StatusCodes.OK).json({ Pricing });
   }
 };
